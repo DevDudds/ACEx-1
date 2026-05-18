8e7eec066332ae837a24c2943493ab0e0e04634d
silvinha21 <arthursilvasoares60@gmail.com>
2026-05-18 13:02:16 -0300

    Refactor(Página de minicursos)

--- cursos mod. Arthur/js/script.js
+++ cursos mod. Arthur/js/script.js
@@ -1,54 +1,66 @@
-const container = document.getElementById("cards_cursos");
-const container2 = document.getElementById("cards_cursos_destaques");
+let cursos = [];
 
-fetch("data/cursos.json")
-  .then((response) => response.json())
-  .then((dados) => {
-    dados.cursos.forEach((curso) => {
-        if (curso.destaque) {
-            const cardDestaque = document.createElement("article");
-            cardDestaque.classList.add("card_curso");
-            
-        cardDestaque.innerHTML = `
+async function carregarCursos(){
+    try {
+        const resposta = await fetch('data/cursos.json');
+        cursos = await resposta.json();
+        gerarCursos(cursos);
+    }catch(erro) {
+        console.log("Erro ao carregar o JSON:", erro);
+    }
+}
+
+carregarCursos();
+
+const filterForm = document.querySelector('.filters__form');
+const inputBusca = document.querySelector('#busca');
+const inputArea = document.querySelector('#area');
+
+filterForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const termoBuscado = inputBusca.value.toLowerCase();
+    const areaEscolhida = inputArea.value.toLowerCase(); 
+
+    const cursosFiltrados = cursos.filter(curso => {
+        const areaCurso = curso.area.toLowerCase();
+        const tituloCurso = curso.titulo.toLowerCase();
+
+        const tituloOk = (tituloCurso.includes(termoBuscado));
+        const areaOk = (areaEscolhida === "" || areaEscolhida === areaCurso);
+
+        return tituloOk && areaOk;
+    })
+
+    gerarCursos(cursosFiltrados);
+})
+
+function gerarCard(curso){
+    return `
+        <article class="card">
             <div class="dados">
                 <img class="img_card" src="${curso.imagem}" alt="Imagem do curso">
-                <span class="card__tag">${curso.categoria}</span>
+                <span class="card__tag">${curso.area}</span>
             </div>
             <div class="card__body">
                 <h3>${curso.titulo}</h3>
                 <p>${curso.descricao}</p>
                 <div class="horas">
-                    <span>${curso.horas}</span>                                
+                    <span>${curso.horas} horas</span>    
                 </div>
-                <a class="card__link" href="${curso.link}">Acessar playlist</a>
-            </div>
-        `;
-            container2.appendChild(cardDestaque);
-        } 
-      const card = document.createElement("article");
-      card.classList.add("card_curso");
-
-      card.innerHTML = `
-        <article class="card_curso">
-            <div class="dados">
-                <img class="img_card" src="${curso.imagem}" alt="Imagem do curso">
-                <span class="card__tag">${curso.categoria}</span>
-            </div>
-            <div class="card__body">
-                <h3>${curso.titulo}</h3>
-                <p>${curso.descricao}</p>
-                <div class="horas">
-                    <span>${curso.horas}</span>                                
-                </div>
-                <a class="card__link" href="${curso.link}">Acessar playlist</a>
+                <a class="card__link" href="#">Acessar playlist</a>
             </div>
         </article>
-            `;
+    `
+}
 
-      container.appendChild(card);
-        
-    });
-  })
-  .catch((error) => {
-    console.error("Erro ao carregar JSON:", error);
-  });
+function gerarCursos(listCursos){
+    const mainCards = document.querySelector('#cards_cursos');
+    const dtqCards = document.querySelector('#cards_cursos_destaques');
+
+    const htmlMainCards = listCursos.map(curso => gerarCard(curso)).join('');
+    mainCards.innerHTML = htmlMainCards;
+
+    const htmlDtqCards = cursos.filter(curso => curso.destaque === true).map(curso => gerarCard(curso)).join('');
+    dtqCards.innerHTML = htmlDtqCards;
+}
